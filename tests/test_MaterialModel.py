cc5dbf77e9f97f3a776031ac323c6615f445b16e
yegt79 <yegt@bu.edu>
2025-02-14 16:47:46 -0500

    Update test_MaterialModel.py

--- tests/test_MaterialModel.py
+++ tests/test_MaterialModel.py
@@ -20,17 +20,17 @@ class IsotropicHardeningModel:
         ep[0] = self.ep_0
 
         for i in range(1, self.n - 1):
-            Y[i] = self.Y_0 + self.H * ep[i]  # Update yield stress
-            sig_trial = sig[i] + self.E * de  # Elastic trial stress
+            Y[i] = self.Y_0 + self.H * ep[i-1]  # Update yield stress
+            sig_trial = sig[i-1] + self.E * de  # Elastic trial stress
             phi_trial = abs(sig_trial) - self.Y_0  # Corrected yield condition
 
             if phi_trial <= 0:  # Elastic step
-                sig[i + 1] = sig_trial
-                ep[i + 1] = ep[i]
+                sig[i] = sig_trial
+                ep[i] = ep[i-1]
             else:  # Plastic step
                 dep = phi_trial / (self.E + self.H)
-                sig[i + 1] = sig_trial - np.sign(sig_trial) * self.E * dep
-                ep[i + 1] = ep[i] + dep
+                sig[i] = sig_trial - np.sign(sig_trial) * self.E * dep
+                ep[i] = ep[i-1] + dep
 
         return sig, ep, Y  # Return the stress, plastic strain, and yield stress
 
@@ -56,22 +56,22 @@ class KinematicHardeningModel:
 
         for i in range(1, self.n - 1):
             # Trial state
-            sig_trial = sig[i] + self.E * de  # Elastic trial stress
-            alpha_trial = alpha[i]  # Backstress remains the same for trial
+            sig_trial = sig[i-1] + self.E * de  # Elastic trial stress
+            alpha_trial = alpha[i-1]  # Backstress remains the same for trial
             etha_trial = sig_trial - alpha_trial  # Effective stress
             phi_trial = abs(etha_trial) - self.Y_0  # Yield condition
 
             # Elastic step
             if phi_trial <= 0:
-                sig[i + 1] = sig_trial
-                alpha[i + 1] = alpha[i]
-                ep[i + 1] = ep[i]
+                sig[i] = sig_trial
+                alpha[i] = alpha[i-1]
+                ep[i] = ep[i-1]
             # Plastic step
             else:
                 dep = phi_trial / (self.E + self.H)  # Plastic strain increment
-                sig[i + 1] = sig_trial - np.sign(etha_trial) * self.E * dep  # Update stress
-                alpha[i + 1] = alpha[i] + np.sign(etha_trial) * self.H * dep  # Update backstress
-                ep[i + 1] = ep[i] + dep  # Increment plastic strain
+                sig[i] = sig_trial - np.sign(etha_trial) * self.E * dep  # Update stress
+                alpha[i] = alpha[i-1] + np.sign(etha_trial) * self.H * dep  # Update backstress
+                ep[i] = ep[i-1] + dep  # Increment plastic strain
 
         return sig, ep, alpha  # Return the stress, plastic strain, and backstress
 
