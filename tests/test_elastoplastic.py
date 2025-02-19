2497038dcf312b7aac7bd938dbf9bfe9013e6bc0
yegt79 <yegt@bu.edu>
2025-02-19 11:03:09 -0500

    Update test_elastoplastic.py

--- tests/test_elastoplastic.py
+++ tests/test_elastoplastic.py
@@ -1,12 +1,10 @@
 import numpy as np
-import pytest
 import sys
 import os
 
-# Add the project root to the Python path
-sys.path.append(os.path.abspath(os.path.join('..')))
-
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from src.elastoplastic import elastoplastic_functions as ef
+
 
 class TestMaterialModels:
     @staticmethod
